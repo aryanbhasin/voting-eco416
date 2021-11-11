d75aa5d9b6a1a9687372d526c31bd3e9f0c214c4
Aryan Bhasin <bhasinaryan@gmail.com>
2021-11-11 17:15:36 -0500

    Finish basic admin platform

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -8,39 +8,192 @@ contract Election {
         uint voteCount;
     }
 
+    struct Voter {
+        bool isRegistered;
+        bool hasVoted;
+        uint votedProposalId;
+    }
+    
+    enum WorkflowStatus {
+        RegisteringVoters,
+        ProposalsRegistrationStarted,
+        ProposalsRegistrationEnded,
+        VotingSessionStarted,
+        VotingSessionEnded,
+        VotesTallied
+    }
+
+    address public administrator;
+
+    WorkflowStatus public workflowStatus;
+
     // Store accounts that have voted
-    mapping(address => bool) public voters;
+    mapping(address => Voter) public voters;
     // Store candidates
     // Fetch candidate
     mapping(uint => Candidate) public candidates;
     // Store candidates count
     uint public candidatesCount;
+    // Store winning candidate's ID
+    uint private winningCandidateId;
 
     // voted event
     event votedEvent (
         uint indexed _candidateId
     );
 
-    // Constructor
-    constructor() public {
-        addCandidate("Candidate 1");
-        addCandidate("Candidate 2");
+    event VoterRegisteredEvent (address voterAddress);
+    event ProposalsRegistrationStartedEvent ();
+    event ProposalsRegistrationEndedEvent ();
+    event ProposalRegisteredEvent (
+        uint proposalId
+    );
+    event VotingSessionStartedEvent ();
+    event VotingSessionEndedEvent ();
+    event VotesTalliedEvent ();
+
+    event WorkflowStatusChangeEvent (
+        WorkflowStatus previousStatus,
+        WorkflowStatus newStatus
+    );
+
+    // ----------------- MODIFIERS -----------------
+    modifier onlyAdministrator() {
+        require(msg.sender == administrator, "The caller of this function must be the administrator!");
+        _;
+    }
+    modifier onlyRegisteredVoter() {
+        require(voters[msg.sender].isRegistered, "The caller of this function must be a registered voter");
+        _;
+    }
+    modifier onlyDuringVotersRegistration() {
+        require(workflowStatus == WorkflowStatus.RegisteringVoters, "This function can be called only during voters registration");
+        _;
     }
 
-    function addCandidate (string memory _name) private {
-        candidatesCount ++;
-        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0); // initialize new Candidate struct 
+    modifier onlyDuringProposalsRegistration() {
+        require(workflowStatus == WorkflowStatus.ProposalsRegistrationStarted, "This function can be called only when proposals registration has started");
+        _;
+    }
+    modifier onlyAfterProposalsRegistration() {
+        require(workflowStatus == WorkflowStatus.ProposalsRegistrationEnded, "This function can be called only when proposals registration has ended");
+        _;
+    }
+
+    modifier onlyDuringVotingSession() {
+        require(workflowStatus == WorkflowStatus.VotingSessionStarted, "This function can be called only when voting session has started");
+        _;
+    }
+    modifier onlyAfterVotingSession() {
+        require(workflowStatus == WorkflowStatus.VotingSessionEnded, "This function can be called only when voting session has ended");
+        _;
+    }
+
+    modifier onlyAfterVotesTallied() {
+        require(workflowStatus == WorkflowStatus.VotesTallied, "This function can be called only after votes have been tallied");
+        _;
     }
     
-    function vote(uint _candidateId) public {
-        require(!voters[msg.sender]); // make sure voter's boolean for hasVoted is false
+    // ----------------- VIEWS (HELPER FUNCTIONS) -----------------
+    function getProposalsNumber() public view returns (uint) {
+        return candidatesCount;
+    }
+
+    function getProposalDescription(uint index) public view returns (string memory) {
+        return candidates[index].name;
+    }
+
+    function getWinningProposalId() 
+        public onlyAfterVotesTallied view returns (uint) {
+        return winningCandidateId;
+    }
+
+    function getWinningProposalDescription()
+        public onlyAfterVotesTallied view returns (string memory) {
+        return candidates[winningCandidateId].name;    
+    }
+
+    function getWinningProposalVoteCounts()
+        public onlyAfterVotesTallied view returns (uint) {
+        return candidates[winningCandidateId].voteCount;
+    }
+
+    function isRegisteredVoter(address _voterAddress) public view returns (bool) {
+        return voters[_voterAddress].isRegistered;
+    }
+
+    function isAdministrator(address _address) public view returns (bool) {
+        return (_address == administrator);
+    }
+
+    function getWorkflowStatus() public view returns (WorkflowStatus) {
+        return workflowStatus;
+    }
+
+    // ----------------- Constructor -----------------
+    constructor() public {
+        administrator = msg.sender;
+        workflowStatus = WorkflowStatus.RegisteringVoters;
+    }
+
+    function registerVoter(address _voterAddress) public onlyAdministrator onlyDuringVotersRegistration {
+        require(!voters[_voterAddress].isRegistered, "the voter is already registered!");
+
+        voters[_voterAddress].isRegistered = true;
+        voters[_voterAddress].hasVoted = false;
+        voters[_voterAddress].votedProposalId = 0;
+        
+        emit VoterRegisteredEvent(_voterAddress);
+    }
+
+    function startProposalsRegistration()
+        public onlyAdministrator onlyDuringProposalsRegistration {
+        workflowStatus = WorkflowStatus.ProposalsRegistrationStarted;
+        emit ProposalsRegistrationStartedEvent();
+        emit WorkflowStatusChangeEvent(WorkflowStatus.RegisteringVoters, workflowStatus);
+    }
+
+    function endProposalsRegistration()
+        public onlyAdministrator onlyAfterProposalsRegistration {
+        workflowStatus = WorkflowStatus.ProposalsRegistrationEnded;
+        emit ProposalsRegistrationEndedEvent();
+        emit WorkflowStatusChangeEvent(WorkflowStatus.ProposalsRegistrationStarted, workflowStatus);
+    }
+
+    function addCandidate (string memory _name) 
+        public onlyAdministrator {
+        candidatesCount ++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0); // initialize new Candidate struct 
+        emit ProposalRegisteredEvent(candidatesCount);
+    }
+    
+    function vote(uint _candidateId) 
+        public onlyRegisteredVoter {
+        require(!voters[msg.sender].hasVoted, "the caller has already voted"); // make sure voter's boolean for hasVoted is false
 
         require(_candidateId > 0 && _candidateId <= candidatesCount);
 
-        voters[msg.sender] = true;
+        voters[msg.sender].hasVoted = true;
+        voters[msg.sender].votedProposalId = _candidateId;
 
         candidates[_candidateId].voteCount ++;
 
         emit votedEvent(_candidateId);
+    }
+
+    function tallyVotes()
+        public onlyAdministrator onlyAfterVotingSession {
+        uint winningVoteCount = 0;
+        uint winningProposalIndex = 0;
+        for (uint i = 0; i < candidatesCount; i++) {
+            if (candidates[i].voteCount > winningVoteCount) {
+                winningVoteCount = candidates[i].voteCount;
+                winningProposalIndex = i;
+            }
+        }
+        winningCandidateId = winningProposalIndex;
+        workflowStatus = WorkflowStatus.VotesTallied;
+        emit VotesTalliedEvent();
+        emit WorkflowStatusChangeEvent(WorkflowStatus.VotingSessionEnded, workflowStatus);
     }
 }
--- src/js/app.js
+++ src/js/app.js
@@ -28,6 +28,7 @@ App = {
       App.contracts.Election.setProvider(App.web3Provider);
 
       App.listenForEvents();
+      App.refreshWorkflowStatus();
 
       return App.render();
     });
@@ -44,7 +45,35 @@ App = {
         // Reload when a new vote is recorded
         App.render();
       })
-    })
+    });
+
+    App.contracts.Election.deployed()
+      .then(instance => instance.WorkflowStatusChangeEvent())
+      .then(workflowStatusChangeEventSubscription => {
+          workflowStatusChangeEvent = workflowStatusChangeEventSubscription;
+          workflowStatusChangeEvent.watch((error, _) => {
+            if (!error) {
+              refreshWorkflowStatus();
+            }
+            else {
+              console.log(error);
+            }
+          })
+      });
+    
+    App.contracts.Election.deployed()
+      .then(instance => instance.ProposalRegisteredEvent())
+      .then(proposalRegisteredEventSubscription => {
+        proposalRegisteredEvent = proposalRegisteredEventSubscription;
+        proposalRegisteredEvent.watch((error, result) => {
+          if (!error) {
+            $("#proposalRegistrationMessage").html("The proposal has been registered successfully");
+            App.render(); // reload when new candidate added
+          } else {
+            console.log(error);
+          }
+        })
+      })
   },
 
   render: function() {
@@ -89,7 +118,8 @@ App = {
         });
       }
       return electionInstance.voters(App.account);
-    }).then(function(hasVoted) {
+    }).then(function(voter) {
+      var hasVoted = voter[1];
       if (hasVoted) {
         $('form').hide();
       }
@@ -98,6 +128,20 @@ App = {
     }).catch(function(error) {
       console.warn(error);
     });
+  },
+
+  loginAsVoter: function() {
+    var voterLoginAddress = $("#voterLoginAddress").val();
+    App.contracts.Election.deployed()
+      .then(instance => instance.isRegisteredVoter(voterLoginAddress))
+      .then(isRegisteredVoter => {
+        if (isRegisteredVoter) {
+          window.location.href="/vote.html";
+        } else {
+          $("#voterLoginMessage").html("Incorrect Voter Login");
+        }
+      })
+      .catch(e => $("#voterLoginMessage").html("Incorrect Voter Login"))
   },
 
   castVote: function() {
@@ -111,6 +155,172 @@ App = {
     }).catch(function(err) {
       console.error(err);
     })
+  },
+
+  unlockVoter: function() {
+    var voterAddress = $("#voterAddress").val();
+    
+    App.contracts.Election.deployed()
+      .then(instance => instance.isRegisteredVoter(voterAddress))
+      .then(isRegisteredVoter => {
+        if (isRegisteredVoter) {
+          $("#voterUnlockedMessage").html("Voter account has been unlocked");
+        } else {
+          $("#voterUnlockedMessage").html("Voter account has NOT been unlocked");
+        }
+        return;
+      })
+      .catch(e => $("#voterUnlockedMessage").html(e))
+
+  },
+
+  // ------------- ADMIN FUNCTIONS -------------
+
+  loginAsAdmin: function() {
+    var adminLoginAddress = $("#adminLoginAddress").val();
+    App.contracts.Election.deployed()
+      .then(instance => instance.isAdministrator(adminLoginAddress))
+      .then(isAdministrator => {
+        if (isAdministrator) {
+          web3.eth.defaultAccount=web3.eth.accounts[0];
+          window.location.href="/admin.html";
+        } else {
+          $("#adminLoginMessage").html("Incorrect Login");
+        }
+      })
+      .catch(e => $("#adminLoginMessage").html("Incorrect Login"))
+  },
+
+  unlockAdmin: function() {
+    var adminAddress = $("#adminAddress").val();
+    // var adminPassword = $("#adminPassword").val();
+    // var result = web3.personal.unlockAccount(adminAddress, adminPassword, 180, function(error, result) {
+    //   if (error) {
+    //     console.log(error);
+    //     $("#adminMessage").html("Admin account has NOT been unlocked");
+    //   } else {
+    //     console.log(result);
+    //     $("#adminMessage").html("Admin account has been unlocked");
+    //   }
+    // })
+    
+    App.contracts.Election.deployed()
+      .then(instance => instance.isAdministrator(adminAddress))
+      .then(isAdministrator => {
+        if (isAdministrator) {
+          $("#adminMessage").html("Admin account has been unlocked");
+        } else {
+          $("#adminMessage").html("Admin account has NOT been unlocked");
+        }
+        return;
+      })
+      .catch(e => $("#adminMessage").html(e))
+
+  },
+
+  registerVoter: function() {
+    $("#voterRegistrationMessage").html("");
+    var adminAddress = $("#adminAddress").val();
+
+    var voterToRegister = $("#voterAddress").val();
+    console.log(web3.eth.defaultAccount);
+    App.contracts.Election.deployed()
+      .then(instance => instance.isAdministrator(adminAddress))
+      .then(isAdministrator => {
+        if (isAdministrator) {
+          return App.contracts.Election.deployed()
+            .then(instance => instance.isRegisteredVoter(voterToRegister))
+            .then(isVoterRegistered => {
+              if (isVoterRegistered) {
+                $("#voterRegistrationMessage").html("Voter is already registered");
+              } else {
+                return App.contracts.Election.deployed()
+                  .then(instance => instance.getWorkflowStatus())
+                  .then(workflowStatus => {
+                    if (workflowStatus > 0) {
+                      $("#voterRegistrationMessage").html("Voter registration has already ended");
+                    }
+                    else {
+                      App.contracts.Election.deployed()
+                        .then(instance => instance.registerVoter(voterToRegister, {from: App.account, gas: 200000}))
+                        .catch(e => $("#voterRegistrationMessage").html(e))
+                    }
+                  })
+              }
+            })
+        } else {
+          $("#voterRegistrationMessage").html("Not logged in as admin");
+        }
+      })
+  },
+
+  checkVoterRegistration: function() {
+    $("#registrationVerificationMessage").html("");
+    var address = $("#checkVoterRegistrationAddress").val();
+    
+    App.contracts.Election.deployed()
+      .then(instance => instance.isRegisteredVoter(address))
+      .then(isRegisteredVoter => {
+        if (isRegisteredVoter) {
+          $("#registrationVerificationMessage").html("Yes, this voter is registered");
+        } else {
+          $("#registrationVerificationMessage").html("No, this voter is NOT registered");
+        }
+      });
+  },
+
+  registerProposal: function() {
+    $("#proposalRegistrationMessage").html('');
+    var adminAddress = $("#adminAddress").val();
+    var proposalName = $("#proposalName").val();
+
+    if (proposalName.length == 0) {
+      $("#proposalRegistrationMessage").html("No proposal name entered");
+    } else {
+      App.contracts.Election.deployed()
+        .then(instance => instance.isAdministrator(adminAddress))
+        .then(isAdministrator => {
+          if (isAdministrator) {
+            App.contracts.Election.deployed()
+              .then(instance => instance.addCandidate(proposalName, {from: App.account, gas: 200000}))
+              .catch(e => $("#proposalRegistrationMessage").html(e))
+          } else {
+            $("#proposalRegistrationMessage").html("You are not logged in as an admin");
+          }
+        })
+    }
+  },
+
+  refreshWorkflowStatus: function() {
+    App.contracts.Election.deployed()
+      .then(instance => instance.getWorkflowStatus())
+      .then(workflowStatus => {
+        var workflowStatusDescription;
+        switch (workflowStatus.toString()) {
+          case '0':
+            workflowStatusDescription = "Registering Voters";
+            break;
+          case '1':
+            workflowStatusDescription = "Proposals Registration Started";
+            break; 
+          case '2':
+            workflowStatusDescription = "Proposals Registration Ended";
+            break; 
+          case '3':
+            workflowStatusDescription = "Voting Session Started";
+            break; 
+          case '4':
+            workflowStatusDescription = "Voting Session Ended";
+            break; 
+          case '5':
+            workflowStatusDescription = "Votes Tallied";
+            break; 
+          default:
+            workflowStatusDescription = "Unknown Status";
+        }
+
+        $("#currentWorkflowStatusMessage").html(workflowStatusDescription);
+      })
   }
 };
 
